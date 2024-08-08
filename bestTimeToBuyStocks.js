b9e01a6551a39a183b7ca9ff3902f1709deefe7d
unknown <you@example.com>
2024-08-08 19:29:44 +0800

    changes

--- bestTimeToBuyStocks.js
+++ bestTimeToBuyStocks.js
@@ -1,9 +1,14 @@
 function maxProfit(prices) {
 
+  /**
+   * Goal is to find the smallest value in array from left to right. But first, set it with
+   * the first element in array.  If the next value of element is lower than previous leastNumber, then set
+   * the leastNumber variable to it and set the maxProjectedProfit[i] to 0.
+   */    
   const maxProjectedProfit = new Array(prices.length);
   let leastNumber = prices[0]
-  maxProjectedProfit[0] = 0
 
+  maxProjectedProfit[0] = 0
   for(i = 1; i < prices.length; i++){
       if(leastNumber > prices[i]){
             leastNumber = prices[i]
@@ -18,4 +23,4 @@ function maxProfit(prices) {
 
 }
 
-console.log(maxProfit([7,6,4,3,1]))
+console.log(maxProfit([5,2,4,1]))
